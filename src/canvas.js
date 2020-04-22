57cedd8992c61a9e01cd11aaab0248b508325ae1
Leon Si <leonzalion@gmail.com>
2020-04-21 21:01:48 -0400

    commit

--- src/canvas.js
+++ src/canvas.js
@@ -1,26 +1,43 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { v4 } from 'uuid';
 import Pusher from 'pusher-js';
 import util from 'util';
 
-class Canvas extends Component {
-  constructor(props) {
-    super(props);
+const pusher = new Pusher('a1de361e8a5975db6810', {
+  cluster: 'us2',
+});
+const userStrokeStyle = '#EE92C2';
+const guestStrokeStyle = '#F0C987';
+const userId = v4();
+const line = [];
 
-    this.pusher = new Pusher('a1de361e8a5975db6810', {
-      cluster: 'us2',
+export default function DrawingCanvas() {
+  const [isPainting, setIsPainting] = useState(false);
+  const [prevPos, setPrevPos] = useState({offsetX: 0, offsetY: 0});
+  
+  useEffect(() => {
+    const canvas = document.querySelector('#drawingCanvas');
+    canvas.width = 200;
+    canvas.height = 200;
+    const ctx = canvas.getContext('2d');
+    ctx.lineJoin = 'round';
+    ctx.lineCap = 'round';
+    ctx.lineWidth = 5;
+
+    const channel = pusher.subscribe('painting');
+    channel.bind('draw', (data) => {
+      const { userId: id, line } = data;
+      if (id !== userId) {
+        line.forEach((position) => {
+          paint(position.start, position.stop, guestStrokeStyle);
+        });
+      }
     });
-  }
-  isPainting = false;
-  userStrokeStyle = '#EE92C2';
-  guestStrokeStyle = '#F0C987';
-  line = [];
-  userId = v4();
-  prevPos = { offsetX: 0, offsetY: 0 };
+  }, []);
 
-  _getOffsets(targetTouches) {
-    const element = document.querySelector('canvas');
-    console.log(util.inspect(element, {showHidden: false, depth: null}))
+  function getOffsets(targetTouches) {
+    const element = document.querySelector('#drawingCanvas');
+    // console.log(util.inspect(element, {showHidden: false, depth: null}))
     const x = element.getBoundingClientRect().x;
     const y = element.getBoundingClientRect().y;
     const offsetX = targetTouches[0].clientX - x;
@@ -28,47 +45,48 @@ class Canvas extends Component {
     return { offsetX, offsetY };
   }
 
-  onTouchStart = ({ targetTouches }) => {
-    this.prevPos = this._getOffsets(targetTouches);
-    this.isPainting = true;
+  function onTouchStart({ targetTouches }) {
+    setPrevPos(getOffsets(targetTouches));
+    setIsPainting(true);
   }
 
-  onTouchMove = ({ targetTouches }) => {
-    if (this.isPainting) {
-      const { offsetX, offsetY } = this._getOffsets(targetTouches);
-      const offSetData = { offsetX, offsetY };
-      this.position = {
-        start: { ...this.prevPos },
-        stop: { ...offSetData },
-      };
-      this.line = this.line.concat(this.position);
-      this.paint(this.prevPos, offSetData, this.userStrokeStyle);
-    }
-  }
-
-  endPaintEvent = () => {
-    if (this.isPainting) {
-      this.isPainting = false;
-      //this.sendPaintData();
-    }
-  }
-
-  paint = (prevPos, currPos, strokeStyle) => {
+  function paint(prevPos, currPos, strokeStyle) {
     const { offsetX, offsetY } = currPos;
     const { offsetX: x, offsetY: y } = prevPos;
 
-    this.ctx.beginPath();
-    this.ctx.strokeStyle = strokeStyle;
-    this.ctx.moveTo(x, y);
-    this.ctx.lineTo(offsetX, offsetY);
-    this.ctx.stroke();
-    this.prevPos = { offsetX, offsetY };
+    const ctx = document.querySelector('#drawingCanvas').getContext('2d');
+    ctx.beginPath();
+    ctx.strokeStyle = strokeStyle;
+    ctx.moveTo(x, y);
+    ctx.lineTo(offsetX, offsetY);
+    ctx.stroke();
+    setPrevPos({ offsetX, offsetY });
   }
 
-  async sendPaintData() {
+  function onTouchMove({ targetTouches }) {
+    if (isPainting) {
+      const { offsetX, offsetY } = getOffsets(targetTouches);
+      const offSetData = { offsetX, offsetY };
+      const position = {
+        start: { ...prevPos },
+        stop: { ...offSetData },
+      };
+      line.push.apply(position);
+      paint(prevPos, offSetData, userStrokeStyle);
+    }
+  }
+
+  function endPaintEvent() {
+    if (isPainting) {
+      setIsPainting(false);
+      // sendPaintData();
+    }
+  }
+
+  async function sendPaintData() {
     const body = {
-      line: this.line,
-      userId: this.userId,
+      line,
+      userId
     };
 
     const req = await fetch('http://tabula-srv.herokuapp.com/paint', {
@@ -80,40 +98,16 @@ class Canvas extends Component {
     });
     const res = await req.json();
     console.log(res);
-    this.line = [];
+    line.splice(0, line.length);
   }
 
-  componentDidMount() {
-    const canvas = document.querySelector('#drawingCanvas');
-    canvas.width = 200;
-    canvas.height = 200;
-    this.ctx = canvas.getContext('2d');
-    this.ctx.lineJoin = 'round';
-    this.ctx.lineCap = 'round';
-    this.ctx.lineWidth = 5;
-
-    const channel = this.pusher.subscribe('painting');
-    channel.bind('draw', (data) => {
-      const { userId, line } = data;
-      if (userId !== this.userId) {
-        line.forEach((position) => {
-          this.paint(position.start, position.stop, this.guestStrokeStyle);
-        });
-      }
-    });
-  }
-
-  render() {
-    return (
-      <canvas
-        id="drawingCanvas"
-        style={{ background: 'black' }}
-        onTouchStart={this.onTouchStart}
-        onTouchMove={this.onTouchMove}
-        onTouchEnd={this.endPaintEvent}
-      />
-    );
-  }
+  return (
+    <canvas
+      id="drawingCanvas"
+      style={{ background: 'black' }}
+      onTouchStart={onTouchStart}
+      onTouchMove={onTouchMove}
+      onTouchEnd={endPaintEvent}
+    />
+  );
 }
-
-export default Canvas;
