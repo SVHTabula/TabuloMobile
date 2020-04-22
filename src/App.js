fb3d94e376c9d67fcfdf8dc6a556a54720b6c40c
AbstractUltra <dragonhe36@gmail.com>
2020-04-23 00:47:12 +0100

    orientation

--- src/App.js
+++ src/App.js
@@ -1,16 +1,35 @@
-import React, { useEffect, useContext, useRef } from 'react';
-import { IonApp } from '@ionic/react';
+import React, { useEffect, useContext, useRef } from "react";
+import { IonApp } from "@ionic/react";
 import SocketContext from "./context/socket";
 import CanvasContext from "./context/canvas";
-import io from 'socket.io-client';
-import './App.css';
-import Canvas from './canvas';
+import io from "socket.io-client";
+import "./App.css";
+import Canvas from "./canvas";
 
-const socket = io('https://tabula-web.herokuapp.com');
+const socket = io("https://tabula-web.herokuapp.com");
 
 export default function App() {
   const lineWidthRef = useRef(5);
-  const lineColorRef = useRef('#ffffff');
+  const lineColorRef = useRef("#ffffff");
+
+  function handleOrientation(event) {
+    var absolute = event.absolute;
+    var alpha = event.alpha;
+    var beta = event.beta;
+    var gamma = event.gamma;
+    if (beta < 170 && beta > 0) {
+      console.log("UP");
+    }
+    if (beta > -170 && beta < 0) {
+      console.log("DOWN");
+    }
+    if (gamma < -30) {
+      console.log('LEFT');
+    }
+    if (gamma > 30) {
+      console.log('RIGHT')
+    }
+  }
 
   useEffect(() => {
     socket.on("setColor", (color) => {
@@ -19,13 +38,14 @@ export default function App() {
     socket.on("setWidth", (width) => {
       lineWidthRef.current = width;
     });
+    window.addEventListener("deviceorientation", handleOrientation, true);
   });
 
   return (
     <IonApp>
       <div className="main">
-        <CanvasContext.Provider value={{lineWidthRef, lineColorRef}}>
-          <SocketContext.Provider value={{socket}}>
+        <CanvasContext.Provider value={{ lineWidthRef, lineColorRef }}>
+          <SocketContext.Provider value={{ socket }}>
             <Canvas />
           </SocketContext.Provider>
         </CanvasContext.Provider>
