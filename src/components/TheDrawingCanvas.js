70c7ae284aad4bb2f592129fa7c65bd25dc7da81
AbstractUltra <dragonhe36@gmail.com>
2020-04-24 01:53:01 +0100

    a

--- src/components/TheDrawingCanvas.js
+++ src/components/TheDrawingCanvas.js
@@ -199,7 +199,7 @@ export default function DrawingCanvas() {
   }
 
   return (
-    <div style={{height: '100%', width: '100%', position: 'absolute', top: 0, right: 0, left: 0, bottom: 0}}>
+    <div style={{height: '100%', width: '100%', position: 'absolute', top: 0, overflow: 'hidden', right: 0, left: 0, bottom: 0}}>
       <canvas
         ref={canvasRef}
         id="drawingCanvas"
